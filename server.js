c704b13ea63d38a47b03fcad0a17359bb8b8727e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-03 16:22:12 +0000

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ let userSchema = new mongoose.Schema({
 let Session = mongoose.model('Session', exerciseSessionSchema);
 let User = mongoose.model('User', userSchema);
 
-app.post('/api/exercise/new-user', (request, response) => {
+app.post('/api/exercise/new-user', bodyParser.urlencoded({extended:false}), (request, response) => {
   let newUser = new User({username: request.body.username});
   newUser.save((error, savedUser) => {
     if(!error) {
@@ -60,7 +60,7 @@ app.get('/api/exercise/users', (request, response) => {
   });
 });
 
-app.post('/api/exercise/add', (request, response) => {
+app.post('/api/exercise/add', bodyParser.urlencoded({extended:false}),(request, response) => {
   let newSession = new Session({
     description: request.body.description,
     duration: parseInt(request.body.duration),
